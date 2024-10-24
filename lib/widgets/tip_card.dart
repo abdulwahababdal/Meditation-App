82ab7ab5bfb67b2cc30c636d76d8b95f04fafaa7
Abdulwahab <abdulwahababdal@gmail.com>
2024-10-24 08:57:59 +0300

    added theme

--- lib/widgets/tip_card.dart
+++ lib/widgets/tip_card.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:meditation_app/models/tip.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(body: TipCard(tip: Tip(text: "text", author: "author"))),
+    theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+      seedColor: MyColors.color5,
+    )),
+    home: Scaffold(
+        body: TipCard(
+      tip: Tip(
+        text: "text",
+        author: "author",
+      ),
+    )),
   ));
+}
+
+class MyColors {
+  static const Color color1 = Color(0xFF5D616B);
+  static const Color color2 = Color(0xFFCDD3D7);
+  static const Color color3 = Color(0xFFE8D8C9);
+  static const Color color4 = Color(0xFFFDF2EE);
+  static const Color color5 = Color(0xFFE5BABD);
 }
 
 class TipCard extends StatelessWidget {
@@ -15,7 +34,8 @@ class TipCard extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Card(
-      child: Column(
+        child: ListTile(
+      title: Column(
         children: [
           TextFormField(
             readOnly: true,
@@ -31,6 +51,6 @@ class TipCard extends StatelessWidget {
           ),
         ],
       ),
-    );
+    ));
   }
 }
