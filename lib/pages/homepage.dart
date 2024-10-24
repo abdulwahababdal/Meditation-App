75169ef950f3c4d1d253885367c6fc1a6b37a5ea
Abdulwahab <abdulwahababdal@gmail.com>
2024-10-24 09:17:01 +0300

    commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,6 +5,12 @@ import 'package:meditation_app/providers/tips_provider.dart';
 import 'package:meditation_app/widgets/tip_card.dart';
 import 'package:provider/provider.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(home: Scaffold(body: Homepage())),
+  );
+}
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
@@ -14,46 +20,54 @@ class Homepage extends StatelessWidget {
       bottomNavigationBar: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(padding: const EdgeInsets.all(8.0) ,child: const Text("Today", textScaler: TextScaler.linear(1.5),)),
-          Container(padding: const EdgeInsets.all(8.0) ,child: const Text("Exercises", textScaler: TextScaler.linear(1.5),)),
+          Container(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                "Today",
+                textScaler: TextScaler.linear(1.5),
+              )),
+          Container(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                "Exercises",
+                textScaler: TextScaler.linear(1.5),
+              )),
         ],
       ),
-      appBar: AppBar(title: const Text("Home Page"),),
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             //TipCard(tip: Tip(text: "text", author: "author")),
             FutureBuilder(
-              future: context.read<TipsProvider>().getTips(), 
-              builder: (context, dataSnapshot) {
-                if (dataSnapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } 
-                else if (dataSnapshot.error != null) {
-                  return const Center(
-                    child: Text('An error occurred'),
-                  );
-                }
-                else {
-                  return //Text("data");
-                  Consumer<TipsProvider>(
-                    builder: (context, provider, child) =>
-                      ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: provider.tips.length,
-                        itemBuilder: (context, index) {
-                          //print("creating card");
-                          return TipCard(tip: provider.tips[index]);
-                        }
-                      )
-                  );
-                }
-              }
-            ),
+                future: context.read<TipsProvider>().getTips(),
+                builder: (context, dataSnapshot) {
+                  if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (dataSnapshot.error != null) {
+                    return const Center(
+                      child: Text('An error occurred'),
+                    );
+                  } else {
+                    return //Text("data");
+                        Consumer<TipsProvider>(
+                            builder: (context, provider, child) =>
+                                ListView.builder(
+                                    shrinkWrap: true,
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    itemCount: provider.tips.length,
+                                    itemBuilder: (context, index) {
+                                      //print("creating card");
+                                      return TipCard(tip: provider.tips[index]);
+                                    }));
+                  }
+                }),
           ],
         ),
       ),
